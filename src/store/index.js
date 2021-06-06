18d0aa6c04cbb91c4a3bc2cbcf10b392e7136ed0
Hariton <yourrasik@gmail.com>
2021-06-06 20:04:12 +1000

    Add: User auth persists

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,6 @@
+/* eslint-disable comma-dangle */
 import { createStore } from 'vuex';
+import { auth, db } from '@/includes/firebase';
 
 export default createStore({
   state: {
@@ -15,5 +17,35 @@ export default createStore({
   },
   getters: {
     // authModalShow: (state) => state.authModalShow,
+  },
+  actions: {
+    async register({ commit }, payload) {
+      const userCred = await auth.createUserWithEmailAndPassword(
+        payload.email,
+        payload.password
+      );
+      await db
+        .collection('users')
+        .doc(userCred.user.uid)
+        .set({
+          name: payload.name,
+          email: payload.email,
+          country: payload.country,
+          age: payload.age,
+        });
+
+      await userCred.user.updateProfile({
+        displayName: payload.name,
+      });
+
+      commit('toggleAuth');
+    },
+    async init_login({ commit }) {
+      const user = auth.currentUser;
+
+      if (user) {
+        commit('toggleAuth');
+      }
+    },
   },
 });
