6b66f0234ed8a3fa5d6594134843de99e2c2dcc0
Hariton <yourrasik@gmail.com>
2021-06-15 11:58:16 +1000

    Add: drag and drop songs

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
+import 'firebase/storage';
 
 // eslint-disable-next-line no-var
 var firebaseConfig = {
@@ -16,6 +17,9 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
+const storage = firebase.storage();
 const usersCollection = db.collection('users');
+const songsCollection = db.collection('songs');
 
-export { auth, db, usersCollection };
+// eslint-disable-next-line object-curly-newline
+export { auth, db, usersCollection, storage, songsCollection };
